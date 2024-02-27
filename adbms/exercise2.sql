c2c3367d6b8a15b0703fcf6506baa08e8dd032b4
akhna <akhnasj@gmail.com>
2024-02-27 15:07:56 +0530

    commit

---
+++ adbms/exercise2.sql
@@ -0,0 +1,11 @@
+-- 01
+SELECT * FROM countries;
+
+-- 02
+SELECT phone_number,email FROM employees;
+
+-- 03
+SELECT * FROM employees WHERE last_name LIKE '%Fay';
+
+-- 04
+SELECT hire_data FROM employees WHERE 
