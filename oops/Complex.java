3a4c457d50d6d033702083945c880181debba5d2
akhna <akhnasj@gmail.com>
2024-02-29 15:36:09 +0530

    commit

---
+++ oops/Complex.java
@@ -0,0 +1,11 @@
+public class Complex {
+    int a, b;
+    
+    public void print() {
+        System.out.println(this.a + " + " + this.b + "i");
+    }
+
+    public void add (Complex c1, Complex c2) {
+        
+    }
+}
