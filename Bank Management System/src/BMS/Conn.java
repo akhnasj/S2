a5e528999c797a587f50080b49f8abeee0fd2d98
Akhna <akhnasj1612@gmail.com>
2024-07-01 21:41:47 +0530

    Connection

---
+++ Bank Management System/src/BMS/Conn.java
@@ -0,0 +1,19 @@
+package BMS;
+
+
+import java.sql.*;
+
+public class Conn {
+    Connection connection;
+    Statement statement;
+    public Conn() {
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "toor");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
