9a16284c2ded8cff94843a8564560f8b7fb76f19
Akhna <akhnasj1612@gmail.com>
2024-07-04 17:34:14 +0530

    Uses Random Class, Local and Global Variables

---
+++ java/VariableScope/Main.java
@@ -0,0 +1,21 @@
+package VariableScope;
+
+public class Main {
+    public static void main(String[] args) {
+        // local = declared inside a method
+        //         visible only to that method
+
+        // global = declared outsite a method, but  within a class
+        //         visisble to all parts of a class
+
+    new DiceRoller();
+    // DiceRoller diceRoller = new DiceRoller();
+
+    }  
+}
+
+// How to run
+
+// javac VariableScope/Main.java VariableScope/DiceRoller.java
+// java VariableScope.Main
+
