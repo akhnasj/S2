c4002d68f49072f8825a78bcd8c69840c79dc56c
Akhna <akhnasj1612@gmail.com>
2024-07-04 20:32:17 +0530

    Static Variable

---
+++ java/StaticKeyword/Friend.java
@@ -0,0 +1,13 @@
+package StaticKeyword;
+
+public class Friend {
+    
+    String name;
+    static int noOfFriends;
+    // int noOfFriends;
+    
+    Friend(String name){
+        this.name = name;
+        noOfFriends ++;
+    }
+}
