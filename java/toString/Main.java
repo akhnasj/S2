9810cd7195d5a9de5073434d142fddc413c43782
Akhna <akhnasj1612@gmail.com>
2024-07-04 18:18:12 +0530

    toString() Overriding

---
+++ java/toString/Main.java
@@ -0,0 +1,25 @@
+package toString;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        // toString() = special method that all objects inherit, 
+        //              that returns a string that "textually represents" an object.
+        //              can be used both implicitly and explicitly
+
+        Car car = new Car();
+
+        // ----- Shows the address of the object
+        // System.out.println(car.toString());
+
+        // ----- As every object inherits the toString() method, when car alone is also 
+        // ----- called, it implicitly calls the toString() method
+        // System.out.println(car);
+
+
+        // So, we override the toString() method to display the textual representation of data
+
+        System.out.println(car.toString());
+        System.out.println(car);
+    }
+}
