7e067d298f3ef95bdf5624bf613e7017dd67f0e1
Akhna <akhnasj1612@gmail.com>
2024-07-05 12:04:39 +0530

    Method Overriding

---
+++ java/MethodOverriding.java
@@ -0,0 +1,17 @@
+import MethodOverriding.Animal;
+import MethodOverriding.Dog;
+
+public class MethodOverriding {
+    public static void main(String[] args) {
+        
+        // method overriding : Declaring a method in sub class,
+        //                     which is already present in parent class.
+        //                     done so that a child class can give its own implementation
+
+        Animal animal = new Animal();
+        animal.speak();
+
+        Dog dog = new Dog();
+        dog.speak();
+    }
+}
---
+++ java/MethodOverriding/Dog.java
@@ -0,0 +1,9 @@
+package MethodOverriding;
+
+public class Dog extends Animal {
+
+    public void speak(){
+        System.out.println("The dog barks");
+    }
+    
+}
