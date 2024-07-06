2ceb1a45474ad5a5289f2cac6d01368903fc3572
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-06 14:37:42 +0530

    break statement

---
+++ NSA/continue.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# skips current iteration / remaining statements of loop
+
+for (( i=0; i<10; i++ ))   # bash
+do 
+    if [ $i -eq 3 ] || [ $i -eq 7 ]
+    then 
+        continue
+    fi
+    echo $i   
+done
+
+
+# doesnot print 3 and 7
