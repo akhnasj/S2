edf45414370412518e42de5cea36fc4f990419fd
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-07 12:34:24 +0530

    Declare Command

--- NSA/declare.sh
+++ NSA/declare.sh
@@ -12,7 +12,6 @@
 
 
 
-# bash
 declare -r pwdfile=/etc/passwd 
 echo $pwdfile
 
@@ -20,3 +19,6 @@ pwdfile=/etc/abc.txt        # error shows that its a read-only variable
 echo $pwdfile               # it echoes the variable value again as before as it is not changed
 
 
+
+
+# bash
