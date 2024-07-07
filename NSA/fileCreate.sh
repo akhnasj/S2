99d6fbd69f004ec47780cb66dbe2bf8d89f290cf
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-07 19:17:31 +0530

    File Operations

---
+++ NSA/fileCreate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# -- Creating files
+
+# touch hello
+
+echo "Enter a file to be created"
+read file
+touch $file
+
+
+
+
+
+
+
+
---
+++ NSA/fileRead.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+# -- Reading a file
+
+echo "Enter the file to read"
+read file
+
+
+if [ -f "$file" ]q
+then 
+	while IFS= read -r line		# IFS is to deal with whitespaces, can also use IFS=""
+	do
+		echo "$line"
+	done < $file
+else
+	echo "$file doesnot exist"
+fi
