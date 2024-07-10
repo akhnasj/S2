acf8fb81c2ae757eb1da77b927a967c465040e6d
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-10 23:00:51 +0530

    Substring of a string

--- NSA/Problems/substring.sh
+++ NSA/Problems/substring.sh
@@ -14,7 +14,7 @@ echo "Substring: $s"
 
 
 
-read -p "Enter a string: " str          # bashs
+read -p "Enter a string: " str          # bash
 read -p "Enter the starting index, and no. of characters: " i n
 
 s=${str:$(( i-1 )):$n}                  # substr=${s:$i:$n}
