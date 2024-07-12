9600849f662e37297e5330588bdbc6af18e1b9e4
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 13:24:33 +0530

    Function to print arguments

--- NSA/Basics/functionPrint.sh
+++ NSA/Basics/functionPrint.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 
-function funPrint() {
-    
+function funPrint() {    
     echo $1
 }
-
 funPrint hi
 
 
-function funPrint() {
-    
-    echo $1 $2 $3 $4    # $@
-}
 
+# or
+
+
+
+function funPrint() {   
+    echo $1 $2 $3 $4           # $@
+}
 funPrint hi this is me 
