7f78cc101639ac1c76edebd717df152ff7e1484a
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 13:37:51 +0530

    Arguments passed to Array

--- NSA/Basics/argArray.sh
+++ NSA/Basics/argArray.sh
@@ -3,15 +3,17 @@
 # $@ takes infinite inputs
 args=("$@")
 
+
 # -- prints out the first two arguments
 # -- ($1) in the input will be assigned index[0] in array(args)
-# echo ${args[0]} ${args[1]}  
+echo ${args[0]} ${args[1]}  
+
 
 # -- prints 2 arguments
  echo $1 $2
 
 # -- prints all arguments
-#echo $@
+echo $@
 
 # -- prints length of array
 echo $#
