6baa80af449c04eeb861b42f3be925ec7dc5f90f
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 13:03:05 +0530

    STDIN

--- NSA/Basics/scriptInput.sh
+++ NSA/Basics/scriptInput.sh
@@ -10,8 +10,10 @@ done < "${1:-/dev/stdin}"
 
 
 
+
 # -- assumes terminal as the file, so prints everything we type in terminal assuming its coming from the file
 # sh file.sh
+
 
 
 # -- prints into the terminal, file we give as argument
