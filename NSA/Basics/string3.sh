33ad4cea310b6cc9f7d3413e291710c766d483f7
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 12:58:09 +0530

    Concatenate Strings

--- NSA/Basics/string3.sh
+++ NSA/Basics/string3.sh
@@ -2,12 +2,23 @@
 
 # Concatenates 2 strings
 
+
 echo "Enter first string"
 read s1
 
 echo "Enter second string"
 read s2
 
-c=$s1$s2
 
+
+echo "$s1$s2"
+
+# or
+
+s1+="$s2"
+echo $s1
+
+# or
+
+c=$s1$s2
 echo $c
