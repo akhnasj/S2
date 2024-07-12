4b482124b8bff6fde28785929b8bd92d5819d494
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 13:48:49 +0530

    Debugging

--- NSA/Basics/debug.sh
+++ NSA/Basics/debug.sh
@@ -5,15 +5,16 @@ bash -x ./filename.sh
 sh -x filename.sh
 
 
-
 # or 
 
-# -- add -x to the shebang (but only works for: )
-./filename.sh
 
+
+# -- add -x to the shebang (but only works for here )
+./filename.sh       
 
 
 # or
+
 
 # -- select from where to where
 set -x
