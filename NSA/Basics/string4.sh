86644bd86076c823a0cc8e759f53075d4c12ca5c
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 12:56:19 +0530

    String to Upper or Lower case

--- NSA/Basics/string4.sh
+++ NSA/Basics/string4.sh
@@ -2,11 +2,13 @@
 
 # Changes the cases of strings
 
+
 echo "Enter a string"
 read s
 
 echo ${s^}      # first letter Uppercase
 echo ${s^^}     # all Uppercase
+
 
 echo ${s,}      # first letter Lowercase
 echo ${s,,}     # all Lowercase
