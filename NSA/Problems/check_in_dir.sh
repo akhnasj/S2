4f7357b4a06df35d308819e510c3698fa9942aa4
Akhna S J <153007938+akhnasj@users.noreply.github.com>
2024-07-12 14:07:11 +0530

    Update check_in_dir.sh

--- NSA/Problems/check_in_dir.sh
+++ NSA/Problems/check_in_dir.sh
@@ -4,6 +4,19 @@
 read -p "Enter the directory: " dir
 direct="/home/dora/Desktop/MCA/$dir"
 
+for i in $(ls)
+do
+    if [ -d $i ]
+    then
+        echo "$i is a directory"
+    else
+        echo "$i is a file"
+    fi
+done 
+
+
+# or
+
 
 for item in "$direct"/*
 do
@@ -16,3 +29,5 @@ do
         echo "$x is a file"
     fi
 done
+
+
