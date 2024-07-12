8178c6f7a4a4a3f3cbc021ce36e0d3e27f9bc70c
AkhnaSJ <akhnasj1612@gmail.com>
2024-07-12 14:57:24 +0530

    Read a file

--- NSA/Basics/fileRead.sh
+++ NSA/Basics/fileRead.sh
@@ -9,10 +9,14 @@ read file
 
 if [ -f "$file" ]
 then 
-	while IFS= read -r line		# IFS is to deal with whitespaces, can also use IFS=""
+	while IFS= read -r line		
 	do
 		echo "$line"
 	done < $file
 else
 	echo "$file doesnot exist"
 fi
+
+
+# IFS is to deal with whitespaces, can also use IFS=""
+# -r to read line by line
