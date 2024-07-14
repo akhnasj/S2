86d883c053913420011ba3aead03442b5ce64d73
Akhna <akhnasj1612@gmail.com>
2024-07-14 17:35:11 +0530

    Deleted Unwanted Files

--- NSA/Basics/scriptTransfer1.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-message="Hello script 1 successfully passed to script 2"
-export message
-
-
-
-./scriptTransfer2.sh
--- NSA/Problems/sys_config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "Current Shell: $SHELL"
-echo "Home Directory: $HOME"
-echo "Operating System Type: $OSTYPE"            # use bash or #!/bin/ksh
-echo -n "Current Working Directory: "
-pwd
-
-
