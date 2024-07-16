c50fa40d746100a3cf6f1d85e2e997a8d61ff732
Akhna <akhnasj1612@gmail.com>
2024-07-16 16:09:12 +0530

    Matrix Addition

--- OOPS/CO1/Matrix.java
+++ OOPS/CO1/Matrix.java
@@ -1,5 +1,74 @@
 import java.util.Scanner;
 
+
+// Using Overloaded Constructors
+public class Matrix {
+    int rows;
+    int cols;
+    int[][] matrix;
+
+
+    public Matrix(Scanner sc){
+        System.out.println("\nEnter the no. of rows and columns: ");
+        this.rows = sc.nextInt();
+        this.cols = sc.nextInt();
+        this.matrix = new int[this.rows][this.cols];
+
+        System.out.println("Enter the values: ");
+        for (int i = 0; i < this.rows; i++ ){
+            for (int j = 0; j < this.cols; j++){
+                this.matrix[i][j] = sc.nextInt();
+            }
+        }
+    }
+
+
+    public Matrix(Matrix matrix1, Matrix matrix2){
+        if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols){
+            System.out.println("Matrices cannot be added!");
+            return;
+        }
+        this.rows = matrix1.rows;
+        this.cols = matrix1.cols;
+        this.matrix = new int[this.rows][this.cols];
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < cols; j++){
+                this.matrix[i][j] = matrix1.matrix[i][j] + matrix2.matrix[i][j];
+            }
+        }
+        display();
+    }
+
+
+    public void display() {
+        System.out.println("\nSum of matrices are: ");
+        for(int i = 0; i < this.rows; i++){
+            for(int j = 0; j < this.cols; j++){
+                System.out.print(this.matrix[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        Matrix matrix1 = new Matrix(sc);
+        Matrix matrix2 = new Matrix(sc);
+
+        new Matrix(matrix1, matrix2);          // Matrix matrix3 = new Mat(matrix1, matrix2)
+
+
+        
+    }
+}
+
+
+
+
+// Using Methods
 public class Matrix {
     int rows;
     int cols;
@@ -69,68 +138,4 @@ public class Matrix {
 
 
 
-// Overloaded Constructors
-
-public class Matrix {
-    int rows;
-    int cols;
-    int[][] matrix;
-
-
-    public Matrix(Scanner sc){
-        System.out.println("\nEnter the no. of rows and columns: ");
-        this.rows = sc.nextInt();
-        this.cols = sc.nextInt();
-        this.matrix = new int[this.rows][this.cols];
-
-        System.out.println("Enter the values: ");
-        for (int i = 0; i < this.rows; i++ ){
-            for (int j = 0; j < this.cols; j++){
-                this.matrix[i][j] = sc.nextInt();
-            }
-        }
-    }
-
-
-    public Matrix(Matrix matrix1, Matrix matrix2){
-        if (matrix1.rows != matrix2.rows || matrix1.cols != matrix2.cols){
-            System.out.println("Matrices cannot be added!");
-            return;
-        }
-        this.rows = matrix1.rows;
-        this.cols = matrix1.cols;
-        this.matrix = new int[this.rows][this.cols];
-        for (int i = 0; i < rows; i++){
-            for (int j = 0; j < cols; j++){
-                this.matrix[i][j] = matrix1.matrix[i][j] + matrix2.matrix[i][j];
-            }
-        }
-        display();
-    }
-
-
-    public void display() {
-        System.out.println("\nSum of matrices are: ");
-        for(int i = 0; i < this.rows; i++){
-            for(int j = 0; j < this.cols; j++){
-                System.out.print(this.matrix[i][j]+" ");
-            }
-            System.out.println();
-        }
-    }
-    
-
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        Matrix matrix1 = new Matrix(sc);
-        Matrix matrix2 = new Matrix(sc);
-
-        new Matrix(matrix1, matrix2);          // Matrix matrix3 = new Mat(matrix1, matrix2)
-
-
-        
-    }
-}
 
