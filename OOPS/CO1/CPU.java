23dd5b2714a6c9671186a1b008e13c95bf797d4e
Akhna <akhnasj1612@gmail.com>
2024-07-16 16:02:04 +0530

    CPU Inner Class

--- OOPS/CO1/CPU.java
+++ OOPS/CO1/CPU.java
@@ -1,11 +1,12 @@
-// 5. Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) and static nested class RAM (memory, manufacturer). Create an object of CPU and print information of Processor and RAM
+// 5. Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) and static nested class RAM (memory, manufacturer). Create an object of CPU and print information of Processor and RAM.
 
 
 public class CPU {
-    
+
     double price;
 
-    public class Processor {
+    // Inner Class
+    class Processor {
         int cores;
         String manufacturer;
 
@@ -13,9 +14,10 @@ public class CPU {
             this.cores = cores;
             this.manufacturer = manufacturer;
         }
+
     }
 
-
+    // Inner Static Class
     static class RAM {
         int memory;
         String manufacturer;
@@ -28,21 +30,21 @@ public class CPU {
 
 
     public static void main(String[] args) {
-        
         CPU cpu = new CPU();
         CPU.Processor processor = cpu.new Processor(4,"Intel");
-        CPU.RAM ram = new CPU.RAM(8,"Digital");
+        CPU.RAM ram = new CPU.RAM(8, "Western Digital");
 
         System.out.println("Processor Information: ");
-        System.out.println("Cores: " + processor.cores);
-        System.out.println("Manufacturer: " + processor.manufacturer);
+        System.out.println("Cores: "+processor.cores);
+        System.out.println("Manufacturer: "+processor.manufacturer);
 
-        System.out.println();
-
-        System.out.println("RAM Information: ");
-        System.out.println("Memory: " + ram.memory);
-        System.out.println("Manufacturer: " + ram.manufacturer);
+        System.out.println("\nRAM Information: ");
+        System.out.println("Memory: "+ram.memory);
+        System.out.println("Manufacturer: "+ram.manufacturer);
+        
+        
+        
+        
     }
 }
-
 
