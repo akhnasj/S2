f563ddc784c995d984d1d8293e2bd58315760746
Akhna <akhnasj1612@gmail.com>
2024-07-16 22:27:31 +0530

    Matrix Addition

--- OOPS/CO1/Matrix.java
+++ OOPS/CO1/Matrix.java
@@ -111,7 +111,7 @@ public class Matrix {
             System.out.println();
         }
     }
-    
+
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -128,6 +128,47 @@ public class Matrix {
         matrix3.displayMatrix();      
         
     }
+}
+
+
+
+// Normal Method
+public class Matrix { 
+    public static void main(String[] args) { 
+        Scanner scanner = new Scanner(System.in); 
+        System.out.println("Enter the number of rows and columns of the matrices:"); 
+        int rows = scanner.nextInt(); 
+        int cols = scanner.nextInt();                                                                                                                      4 
+ 
+
+        int[][] matrix1 = new int[rows][cols]; 
+        System.out.println("Enter the elements of the first matrix:"); 
+        for (int i = 0; i < rows; i++)  
+            for (int j = 0; j < cols; j++)  
+                matrix1[i][j] = scanner.nextInt(); 
+
+        int[][] matrix2 = new int[rows][cols]; 
+        System.out.println("Enter the elements of the second matrix:"); 
+        for (int i = 0; i < rows; i++)  
+            for (int j = 0; j < cols; j++)  
+                matrix2[i][j] = scanner.nextInt(); 
+
+        int[][] sum = new int[rows][cols]; 
+        for (int i = 0; i < rows; i++)  
+            for (int j = 0; j < cols; j++)  
+                sum[i][j] = matrix1[i][j] + matrix2[i][j]; 
+        
+
+ 
+        System.out.println("Sum of the matrices:"); 
+        for (int i = 0; i < rows; i++) {  
+            for (int j = 0; j < cols; j++)  
+                System.out.print(sum[i][j] + " "); 
+            System.out.println(); 
+        } 
+        scanner.close(); 
+    } 
+          
 }
 
 
