e3bdb874c841aed329ae50bc4e0a1843755c74fe
Akhna <akhnasj1612@gmail.com>
2024-07-17 14:05:03 +0530

    CPU Inner Classes

--- OOPS/CO1/CPU.java
+++ OOPS/CO1/CPU.java
@@ -1,4 +1,4 @@
-// 5. Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) and static nested class RAM (memory, manufacturer). Create an object of CPU and print information of Processor and RAM.
+// Create CPU with attribute price. Create inner class Processor (no. of cores, manufacturer) and static nested class RAM (memory, manufacturer). Create an object of CPU and print information of Processor and RAM.
 
 
 public class CPU {
