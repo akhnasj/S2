e47c2f5eba73c3a28c5cd5820e8d0467573bf403
Akhna <akhnasj1612@gmail.com>
2024-07-18 12:13:28 +0530

    Super Keyword

--- OOPS/SuperKeyword.java
+++ OOPS/SuperKeyword.java
@@ -7,9 +7,9 @@ public class SuperKeyword {
         //         very similar to the 'this' keyword
 
         Hero hero1 = new Hero("Batman", 42, "$$$");
-        // System.out.println(hero1.name);
-        // System.out.println(hero1.age);
-        // System.out.println(hero1.power);
+        System.out.println(hero1.name);
+        System.out.println(hero1.age);
+        System.out.println(hero1.power);
 
         Hero hero2 = new Hero("Superman", 43, "everything");
         System.out.println(hero2.toString());;
